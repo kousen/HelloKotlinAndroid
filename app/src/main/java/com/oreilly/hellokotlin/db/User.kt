9cdf0606c2756b9343f0dc3d8886a3b9e990fd5f
Ken Kousen <ken.kousen@kousenit.com>
2022-04-14 23:11:10 -0400

    Reformat code and add comments

--- app/src/main/java/com/oreilly/hellokotlin/db/User.kt
+++ app/src/main/java/com/oreilly/hellokotlin/db/User.kt
@@ -1,14 +1,12 @@
 package com.oreilly.hellokotlin.db
 
-import android.content.Context
-import androidx.annotation.WorkerThread
 import androidx.room.*
 
 @Entity(tableName = "users")
 data class User(
-        @ColumnInfo(name = "name")
-        val name: String,
+    @ColumnInfo(name = "name")
+    val name: String,
 
-        @PrimaryKey(autoGenerate = true)  // last, so can omit it when instantiating
-        var _id: Long = 0L
+    @PrimaryKey(autoGenerate = true)  // last, so can omit it when instantiating
+    var _id: Long = 0L
 )
